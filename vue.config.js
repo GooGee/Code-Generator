a1bc7d8958d49c3c910e6c8b0b1669cf0cacd1ba
GooGee <mou-gj@live.com>
2020-10-28 14:15:18 +0800

    + outputDir in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
+function getFolder() {
+    const list = process.env.VUE_APP_VERSION.split('.')
+    const minor = list[1] >= 10 ? list[1] : '0'.concat(list[1])
+    return 'dist' + list[0] + minor
+}
+
 module.exports = {
+    outputDir: getFolder(),
     publicPath: '',
     chainWebpack: config => {
         config.resolve.extensions.merge(['.ts']).end()
