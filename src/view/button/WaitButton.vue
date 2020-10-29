652946e1ebc9748a7c1f9bbfa987cb96c563e0ad
GooGee <mou-gj@live.com>
2020-10-29 09:41:23 +0800

    + WaitButton.vue

---
+++ src/view/button/WaitButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-button @click="$emit('run')" :disabled="waiting" variant="outline-success">
+        <b-spinner v-if="waiting" small></b-spinner>
+        <slot></slot>
+    </b-button>
+</template>
+
+<script>
+export default {
+    name: 'WaitButton',
+    props: {
+        waiting: {
+            type: Boolean,
+            required: true,
+        },
+    },
+}
+</script>
