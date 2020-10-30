2278efa585306f2e2823138850867d1d81e49826
GooGee <mou-gj@live.com>
2020-10-30 10:59:37 +0800

    + EditButton.vue

---
+++ src/view/button/EditButton.vue
@@ -0,0 +1,43 @@
+<template>
+    <WaitButton @run="run" :waiting="waiting"> Edit </WaitButton>
+</template>
+
+<script>
+import WaitButton from './WaitButton.vue'
+import sss from '@/state.js'
+
+export default {
+    name: 'EditButton',
+    components: {
+        WaitButton,
+    },
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            waiting: false,
+        }
+    },
+    methods: {
+        run() {
+            if (this.waiting) {
+                return
+            }
+
+            this.waiting = true
+            sss.bridge.edit(this.title, this.content, (ok, text) => {
+                this.waiting = false
+                this.$emit('save', ok, text)
+            })
+        },
+    },
+}
+</script>
