0f15311770c98c515c60d734d5e3821a997e0af3
GooGee <mou-gj@live.com>
2020-11-04 13:47:57 +0800

    + lodash in JSParameter.ts

--- src/model/JSParameter.ts
+++ src/model/JSParameter.ts
@@ -1,3 +1,4 @@
+import { LoDashStatic } from 'lodash'
 import Entity from './Schema/Entity'
 import Layer from './Schema/Layer'
 import Project from './Schema/Project'
@@ -5,5 +6,6 @@ import Project from './Schema/Project'
 export interface JSParameter {
     entity: Entity
     layer: Layer
+    lodash: LoDashStatic
     project: Project
 }
