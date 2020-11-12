c08bf24154ca58d758bcacfb3d60977b5ecf5765
GooGee <mou-gj@live.com>
2020-11-12 09:03:41 +0800

    + ColorButton.vue

---
+++ src/view/button/ColorButton.vue
@@ -0,0 +1,55 @@
+<template>
+    <span
+        :style="'background-color:' + color"
+        class="color-button"
+        :class="{ active: active, empty: color === '', white: color === '#fff' }"
+    ></span>
+</template>
+
+<script>
+export default {
+    name: 'ColorButton',
+    props: {
+        color: {
+            type: String,
+            required: true,
+        },
+        active: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+    },
+}
+</script>
+
+<style>
+.color-button {
+    display: inline-block;
+    border-radius: 50%;
+    cursor: pointer;
+    width: 33px;
+    height: 33px;
+    margin: 3px;
+}
+
+.color-button.empty,
+.color-button.white {
+    border: solid 1px lightgray;
+}
+
+.color-button.active {
+    border: solid 5px black;
+}
+
+.color-button.empty {
+    background: linear-gradient(
+        to top left,
+        rgba(0, 0, 0, 0) 0%,
+        rgba(0, 0, 0, 0) calc(50% - 3px),
+        #f00 50%,
+        rgba(0, 0, 0, 0) calc(50% + 3px),
+        rgba(0, 0, 0, 0) 100%
+    );
+}
+</style>
--- src/view/button/ColorPanel.vue
+++ src/view/button/ColorPanel.vue
@@ -1,20 +1,19 @@
 <template>
-    <div id="ColorPanel" style="width: 244px;">
-        <span @click="$emit('update:color', '')" :class="{ active: '' === color }" class="button empty"></span>
-        <span
-            v-for="item in list"
-            :key="item"
-            :style="'background-color:' + item"
-            @click="$emit('update:color', item)"
-            class="button"
-            :class="{ active: item === color, white: item === '#fff' }"
-        ></span>
+    <div style="width: 244px;">
+        <span v-for="item in list" :key="item" @click="$emit('update:color', item)">
+            <ColorButton :color="item" :active="item === color"></ColorButton>
+        </span>
     </div>
 </template>
 
 <script>
+import ColorButton from './ColorButton.vue'
+
 export default {
     name: 'ColorPanel',
+    components: {
+        ColorButton,
+    },
     props: {
         color: {
             type: String,
@@ -23,43 +22,8 @@ export default {
     },
     data() {
         return {
-            list: ['#f00', '#f80', '#ff0', '#80f', '#f0f', '#fff', '#0f0', '#0ff', '#00f', '#08f', '#888'],
+            list: ['', '#f00', '#f80', '#ff0', '#80f', '#f0f', '#fff', '#0f0', '#0ff', '#00f', '#08f', '#888'],
         }
     },
 }
 </script>
-
-<style>
-#ColorPanel .button {
-    display: inline-block;
-    border-radius: 50%;
-    cursor: pointer;
-    width: 33px;
-    height: 33px;
-    margin: 3px;
-}
-
-#ColorPanel .button.white {
-    border: solid 1px lightgray;
-}
-
-#ColorPanel .button.active {
-    border: solid 5px black;
-}
-
-#ColorPanel .button.empty {
-    border: solid 1px lightgray;
-    background: linear-gradient(
-        to top left,
-        rgba(0, 0, 0, 0) 0%,
-        rgba(0, 0, 0, 0) calc(50% - 3px),
-        #f00 50%,
-        rgba(0, 0, 0, 0) calc(50% + 3px),
-        rgba(0, 0, 0, 0) 100%
-    );
-}
-
-#ColorPanel .button.empty.active {
-    border: solid 5px black;
-}
-</style>
