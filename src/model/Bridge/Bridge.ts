43999263b3654e39a4d54bc6c51a870d4955bb06
GooGee <mou-gj@live.com>
2021-01-30 12:15:04 +0800

    + isHTTP in Bridge.ts

--- src/model/Bridge/Bridge.ts
+++ src/model/Bridge/Bridge.ts
@@ -1,3 +1,4 @@
+import { ActionEnum } from './ActionEnum'
 import HandlerManager from './HandlerManager'
 import IResponse from './IResponse'
 
@@ -15,10 +16,27 @@ export default class Bridge {
         console.log(`${response.status} ${response.action} ${response.key} ${response.message}`)
         const handler = this.manager.find(response.action, response.key)
         if (handler) {
-            handler(response)
+            try {
+                if (this.isHTTP(response.action)) {
+                    if (response.data.length > 0) {
+                        const data = JSON.parse(response.data)
+                        return handler(data)
+                    }
+                }
+                handler(response)
+            } catch (error) {
+                console.error(error)
+            }
         } else {
             console.error('No handler')
         }
+    }
+
+    isHTTP(action: ActionEnum) {
+        if (action === ActionEnum.get || action === ActionEnum.post) {
+            return true
+        }
+        return false
     }
 
     error(code: number, message: string) {
