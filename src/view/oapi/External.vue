670b0f99a829c521e3eba647a7eeaf4a918a5739
GooGee <mou-gj@live.com>
2021-02-05 11:55:18 +0800

    + External.vue

---
+++ src/view/oapi/External.vue
@@ -0,0 +1,30 @@
+<template>
+    <table class="table">
+        <tbody>
+            <tr>
+                <td class="text-right" width="222px">description</td>
+                <td>
+                    <b-form-input v-model="item.description"></b-form-input>
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">url</td>
+                <td>
+                    <b-form-input v-model="item.url"></b-form-input>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'External',
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
