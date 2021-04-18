8cc4eef1284d36e6685bedd65ddb7946b140a396
GooGee <mou-gj@live.com>
2021-04-18 15:16:45 +0800

    + upgrade in Loader.ts

--- src/model/Service/Loader.ts
+++ src/model/Service/Loader.ts
@@ -1,11 +1,11 @@
 import UniqueItem from '../Base/UniqueItem'
 import UniqueList from '../Base/UniqueList'
+import Folder from '../Schema/Folder'
 import Project from '../Schema/Project'
 
 const MinVersion = 20
 
 export default class Loader {
-
     static load(source: Project, preset: Project) {
         if (!this.isProject(source)) {
             throw new Error('Unknown data!')
@@ -28,20 +28,37 @@ export default class Loader {
         return project
     }
 
+    static upgrade(source: Project, preset: Project) {
+        source.version = MinVersion
+        return this.load(source, preset)
+    }
+
     private static loadPreset(source: Project, preset: Project) {
         const project = new Project(source.name)
         source.version = project.version
         project.load(source)
         this.addIfNotExist(project.presetManager, preset.presetManager)
         this.addIfNotExist(project.artisanManager, preset.artisanManager)
+        this.loadFolder(project.folder, preset.folder)
         return project
     }
 
     private static addIfNotExist(manager: UniqueList<UniqueItem>, preset: UniqueList<UniqueItem>) {
         preset.list.forEach(data => {
             if (manager.find(data.name) === undefined) {
-                manager.add(data)
+                const item = manager.make(data.name)
+                item.load(data)
+                manager.add(item)
             }
+        })
+    }
+
+    private static loadFolder(folder: Folder, preset: Folder) {
+        this.addIfNotExist(folder.folderManager, preset.folderManager)
+        this.addIfNotExist(folder.layerManager, preset.layerManager)
+        preset.folderManager.list.forEach(item => {
+            const found = folder.folderManager.find(item.name)
+            this.loadFolder(found!, item)
         })
     }
 
